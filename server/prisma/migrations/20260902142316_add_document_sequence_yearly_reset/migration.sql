2449560c94aca954f82c2dd388e940153b2ba5a1
Ange Aurele TUYISENGE <tuyisengeauris@gmail.com>
2026-09-02 16:27:40 +0200

    add yearly reset fields to DocumentSequence model

---
+++ server/prisma/migrations/20260902142316_add_document_sequence_yearly_reset/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Customer" ALTER COLUMN "portalToken" SET DEFAULT gen_random_uuid()::text;
+
+-- AlterTable
+ALTER TABLE "Document" ALTER COLUMN "publicToken" SET DEFAULT gen_random_uuid()::text;
+
+-- AlterTable
+ALTER TABLE "DocumentSequence" ADD COLUMN     "lastResetYear" INTEGER,
+ADD COLUMN     "resetYearly" BOOLEAN NOT NULL DEFAULT false;
